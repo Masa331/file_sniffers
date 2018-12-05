75c840f1a10e088fe54419f77e6d961a32f48ccb
Premysl Donat <pdonat@seznam.cz>
2018-12-05 20:36:39 +0100

    bundle new

---
+++ lib/file_sniffers.rb
@@ -0,0 +1,6 @@
+require "file_sniffers/version"
+
+module FileSniffers
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/file_sniffers/version.rb
@@ -0,0 +1,3 @@
+module FileSniffers
+  VERSION = "0.1.0"
+end
